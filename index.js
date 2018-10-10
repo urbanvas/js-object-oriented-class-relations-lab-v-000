6315de7eb911895dfb607e99b4c5022f9de03cd4
Neil Seejoor <nvseejoor@gmail.com>
2018-10-10 19:47:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ class Trip {
     store.trips.push(this)
   }
   passenger () {
-    return store.passengers.filter(function (passanger) {
+    return store.passengers.filter(function (passenger) {
         return passenger.id === this.id
       }.bind(this)
     )
