689e97e4a7ef9f0305644d7a50e9e7ff3eec0f47
Neil Seejoor <nvseejoor@gmail.com>
2018-10-10 19:52:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -27,10 +27,10 @@ class Trip {
     return store.passengers.find(function (passenger) {
         return passenger.id === this.passengerId
       }.bind(this)
-    )}
+  )}
   driver () {
     return store.drivers.find(function (driver) {
       return driver.id === this.driverId
-    }.bind(this))
-  }
+    }.bind(this)
+  )}
 }
