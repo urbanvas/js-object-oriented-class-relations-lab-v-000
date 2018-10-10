78b62a62fdb6b82f4b3ce69d39a2286a310c8828
Neil Seejoor <nvseejoor@gmail.com>
2018-10-10 19:22:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 let store = {drivers: []}
-
+let driverId = 0
 class Driver {
-  constructor (name, id) {
+  constructor (name) {
     this.name = name
-    this.id = id
+    this.id = ++driverId
     store.drivers.push(this)
   }
 }
